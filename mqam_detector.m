b3b37fc54b30037272cfd8b9a2e82624e8fb04e9
G-ALI007 <aghader563@gmail.com>
2026-03-17 16:26:39 +0300

    Add files via upload

---
+++ mqam_detector.m
@@ -0,0 +1,13 @@
+function [z]= mqam_detector(r,M)
+ %Function to detect MQAM modulated symbols
+ %[dCap]= mqam_detector(M,r) detects the received MQAM signal points
+ %points - 'r'. M is the modulation level of MQAM
+ if(((M~=1) && ~mod(floor(log2(M)),2))==0) %M not a even power of 2
+ error('Only Square MQAM supported. M must be even power of 2');
+ end
+ ref=constructQAM(M); %reference constellation for MQAM
+ x=[real(r); imag(r)]'; %received vec. in cartesian form
+ y=[real(ref); imag(ref)]';%reference vec. in cartesian form
+ [~,z]= minEuclideanDistance(x,y);
+
+end
